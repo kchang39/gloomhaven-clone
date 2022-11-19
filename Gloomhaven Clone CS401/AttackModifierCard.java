2211434488e7be15fb8316ec1c4aa68feb5af706
Kamaljeet Ram <55729964+KamaljeetRam1000@users.noreply.github.com>
2022-11-19 10:38:47 -0800

    Add files via upload

---
+++ Gloomhaven Clone CS401/AttackModifierCard.java
@@ -0,0 +1,7 @@
+//In this class, we will be making a class that contains the title of the card
+package DeckOfCards;
+public class AttackModifierCard {
+	//Declare a method that contains the title of the card.
+	//
+
+}
