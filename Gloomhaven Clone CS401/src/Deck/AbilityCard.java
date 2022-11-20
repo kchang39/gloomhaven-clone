60e28c7e384f8c76455f47cbc511e43547392f9e
Kamaljeet Ram <55729964+KamaljeetRam1000@users.noreply.github.com>
2022-11-20 12:34:31 -0800

    Update AbilityCard.java

--- Gloomhaven Clone CS401/src/Deck/AbilityCard.java
+++ Gloomhaven Clone CS401/src/Deck/AbilityCard.java
@@ -1,5 +1,42 @@
 package Deck;
 
 public class AbilityCard {
+	//Pseudocode
+	//1. Declare some variables related to the card.
+    //2. Setter and getter functions for the variables or constructors
+	//3. Display card info. 
+	//4. 
 
+	private String Name;//Name of the attack
+	private int initNum;//Initializer number.
+	private int levelNum;//The level of the attack card.
+	private String topAction;//The top action of the card.
+	private String bottomAction;//The bottom action of the card.
+	private String sepAbilities;//Separe abilities of the card. Located at the bottom.
+	
+  //Constructor used to initialize the variables.
+	public AbilityCard(String N, int IN, int ln, String tA, String bA, String sA) {
+		Name = N;
+		initNum = IN;
+		levelNum = ln;
+		topAction = tA;
+		bottomAction = bA;
+		sepAbilities = sA;
+		
+	}
+  //Displays all the information on the card.
+	public void ShowAttackCard() {
+		System.out.println("Name: " + Name);
+	    System.out.println("Initializer Number: " + initNum);
+	    	
+	    System.out.println("Level Number: " + levelNum);
+	    System.out.println("Top Action: " + topAction);
+	    
+	    System.out.println("Bottom Action: " + bottomAction);
+	    System.out.println("Separate abilities: " + sepAbilities);
+		
+	    
+	}
+
+	
 }
