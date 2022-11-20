467992962082e71b78e069306c92404411bc808c
Kamaljeet Ram <55729964+KamaljeetRam1000@users.noreply.github.com>
2022-11-20 13:47:36 -0800

    Update AttackModifierCard.java

--- Gloomhaven Clone CS401/src/Deck/AttackModifierCard.java
+++ Gloomhaven Clone CS401/src/Deck/AttackModifierCard.java
@@ -1,7 +1,25 @@
 //In this class, we will be making a class that contains the title of the card
 package Deck;
 public class AttackModifierCard {
-	//Declare a method that contains the title of the card.
-	//
-
+	//Attack modifier cards have a symbol that denote the attack boost
+	//Print out a messsage telling the player/user how much attack boost they
+	
+	
+	private String Title;//The title of the attack modifier card.
+	private String AttackBoost;//The attack boost symbol that is used to determine how much the attack is boosted.
+	private String Type;//The 
+	
+	//Constructor for AttackModifierCard class used to initialize variables.
+	public AttackModifierCard(String t, String a, String ty) {
+		Title = t;
+		AttackBoost = a;
+		Type = ty;
+	}
+	
+	//Displays the information on the card.
+	public void showAttackModifierCar() {
+		System.out.println("Card Title:" + Title);
+		System.out.println("Attack Boost number:" + AttackBoost);
+		System.out.println("Card type symbol:" + Type);
+	}
 }
