47e5925cb275c57a09b61548dc0838e744423c25
Kamaljeet Ram <55729964+KamaljeetRam1000@users.noreply.github.com>
2022-11-20 22:35:01 -0800

    Update BattleGoalCard.java

--- Gloomhaven Clone CS401/src/Deck/BattleGoalCard.java
+++ Gloomhaven Clone CS401/src/Deck/BattleGoalCard.java
@@ -1,5 +1,18 @@
 package Deck;
 
+package DeckOfCards;
 public class BattleGoalCard {
+	private int numOfCheckMarks;
+	private String Prompt;
+	
+	public BattleGoalCard(int c, String p) {
+		numOfCheckMarks = c;
+		Prompt = p;
+	}
 
+	void DisplayBattleGoal() {
+		System.out.println("Battle Goal: " + Prompt);
+		System.out.println("Checkmarks: " + numOfCheckMarks);
+	}
 }
+
