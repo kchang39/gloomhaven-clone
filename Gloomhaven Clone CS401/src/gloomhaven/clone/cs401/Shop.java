50d99765124e70379bff110dcd5e00e2054f48f0
kchang39 <kchang39@horizon.csueastbay.edu>
2022-11-25 14:14:45 -0800

    updated functions for item and shop

--- Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/Shop.java
+++ Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/Shop.java
@@ -15,4 +15,37 @@ public class Shop {
     public Shop(Item item) {
         allItemList.add(item);
     }
+
+    //lets player buy an item from the shop, adds it to their deck, then removes the item from the shop
+    //no player can own duplicate item
+    public void buyItem(Item item) {    //need to pass in player object?
+        //check if player deck has item already
+        for (int i = 0; i < allItemList.size(); i ++) {
+            if (allItemList.get(i) == item) {
+                //give player item
+                //check if they have enough gold
+                //takes the gold from their inventory
+                //remove item from shop
+                System.out.println("Bought item " + allItemList.get(i).getName() + " for: ");  //notifies user what item was bought and for how much gold
+                allItemList.remove(i);
+                
+                return;
+            }
+        }
+    }
+
+    public void sellItem(Item item) {
+        allItemList.add(item);
+        //gives player half the price listed on the item(rounded down) 
+        //item.getGoldCost()/2
+        //probably return that number^^ 
+        //System.out.println("Sold item for: " + *price*);
+    }
+
+    //when a character retires, all their item cards go back to the shop
+    public void retireCharacter(Deck deck) {
+        //iterates through list of item cards the player owns
+        //adds each card to the shop
+    }
+
 }
