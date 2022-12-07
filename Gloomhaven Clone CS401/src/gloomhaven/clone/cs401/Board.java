fc450fd24351311a038cca0a061f866b1937fcfe
Aaron Smith <33908812+pishSandwich@users.noreply.github.com>
2022-12-07 14:52:18 -0800

    Update Board.java

--- Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/Board.java
+++ Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/Board.java
@@ -6,132 +6,55 @@ import java.util.ArrayList;
 class boardLocation{
     
     private int pieceType = 0;
-    private int player;
-    private Enemy enemy;
-    private int x;
-    private int y;
+    //Piece type should use 0 for empty, 1 for player, and 2 for enemy
+    private int party;
     
-    public void addX(int xCoord){
-        x = xCoord;
-    }
-    
-    public void addY(int yCoord){
-        y = yCoord;
-    }
-    
-    public void addPlayer(int play){
-        
-        player = play;
-        pieceType = 1;
-        
-    }
-    
-    public void addEnemy(Enemy enem){
-        
-        enemy = enem;
-        pieceType = 2;
-        
-    }
-    
-    public void removeObject(){
-        
-        player = -1;
-        enemy = null;
-        pieceType = 0;
-        
+    public void changePieceType(int piece){
+        pieceType = piece;
     }
     
     public int getPieceType(){
-        
         return pieceType;
-        
     }
     
-    public int getPlayer(){
-        
-        return player;
-        
+    public void changePartyMember(int member){
+        party = member;
     }
     
-    public Enemy getEnemy(){
-        
-        return enemy;
-        
-    }
-    
-    public int getX(){
-        return x;
-    }
-    
-    public int getY(){
-        return y;
+    public int getPartyMember(){
+        return party;
     }
     
 }
 
 public class Board {
     
-    private boardLocation[] Tiles = new boardLocation[32];
+    private boardLocation[][] Tiles = new boardLocation[12][12];
     
     public Boolean Move(int before, int after){
         
         Boolean validMove = true;
         
-        int check = Tiles[after].getPieceType();
-        if(check == 0){
-            
-            check = Tiles[before].getPieceType();
-            if(check == 1){
-                int play = Tiles[before].getPlayer();
-                Tiles[after].addPlayer(play);
-                Tiles[before].removeObject();
-            }else if(check == 2){
-                Enemy enemy = Tiles[before].getEnemy();
-                Tiles[after].addEnemy(enemy);
-                Tiles[before].removeObject();
-            }
-            
-        }else{
-            validMove = false;
-        }
+        
         
         return validMove;
     }
     
-    public void updateTilePlayer(int player, int tile){
-        
-        Tiles[tile].addPlayer(player);
-        
-    }
-    
-    public void updateTileEnemy(Enemy enemy, int tile){
-        
-        Tiles[tile].addEnemy(enemy);
+    public void updateTile(int x, int y, int type, int party){
+        //Use type = 0 for empty tile, type = 1 for player, type = 2 for enemy
+        Tiles[x][y].changePieceType(type);
+        Tiles[x][y].changePartyMember(party);
         
     }
     
-    public void deadObject(int tile){
-        
-        Tiles[tile].removeObject();
-        
-    }
-    
-    public int CheckObjectTypeInTile(int tile){
-        
-        return Tiles[tile].getPieceType();
+    public int CheckObjectTypeInTile(int x, int y){
+        //Should return 0 for empty tile, 1 for player, and 2 for enemy
+        return Tiles[x][y].getPieceType();
         
     }
     
-    public int checkPlayer(int tile){
-        
-        return Tiles[tile].getPlayer();
-        
-    }
-    
-    public Enemy checkEnemy(int tile){
-        
-        return Tiles[tile].getEnemy();
-        
+    public int CheckPartyMember(int x, int y){
+        return Tiles[x][y].getPartyMember();
     }
     
 }
