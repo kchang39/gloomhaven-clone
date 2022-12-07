fec88a07d1a1747aa2c93a91d78cda99f1a698b2
Aaron Smith <33908812+pishSandwich@users.noreply.github.com>
2022-12-07 14:57:54 -0800

    Update PrintBoard.java

--- Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/PrintBoard.java
+++ Gloomhaven Clone CS401/src/gloomhaven/clone/cs401/PrintBoard.java
@@ -4,41 +4,26 @@ public class PrintBoard {
     
     public void Print(Board board){
         
-        Boolean pattern = true;
-        int pattern1 = 0;
-        int col = 2;
-        
-        for (int k = 0; k < 7; k++){
-            System.out.print(k+1);
-            System.out.print(" ");
+        for (int k = 0; k < 12; k++){
+            System.out.print(k);
+            System.out.print("  ");
         }
         
         System.out.print("\n1");
         
-        for(int i = 0; i < 32; i++){
-            if(pattern == true && pattern1 == 4){
-                System.out.print("\n ");
-                pattern = false;
-                pattern1 = 0;
-                System.out.print(col);
-                col++;
-            }else if (pattern == false && pattern1 == 3){
-                System.out.println();
-                pattern = true;
-                pattern1 = 0;
-                System.out.print(col);
-                col++;
+        for(int i = 0; i < 12; i++){
+            System.out.print(i);
+            System.out.print("  ");
+            for(int h = 0; h< 12; h++){
+            int j = board.CheckObjectTypeInTile(i, h);
+                if(j == 0){
+                    System.out.print("⬡  ");
+                }else if(j == 1){
+                    System.out.print("P  ");
+                }else if(j == 2){
+                    System.out.print("E  ");
+                }
             }
-            
-            int j = board.CheckObjectTypeInTile(i);
-            if(j == 0){
-                System.out.print("⬡  ");
-            }else if(j == 1){
-                System.out.print("P  ");
-            }else if(j == 2){
-                System.out.print("E  ");
-            }
-            pattern1++;
         }
         
     }
